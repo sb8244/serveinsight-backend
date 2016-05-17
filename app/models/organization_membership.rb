ec172b3904956144bc60adf5ce233cf7244c0f1d
Stephen Bussey <steve.bussey@salesloft.com>
2016-05-16 23:17:00 -0400

    define relations for reviewer and direct reports
    Ran into all sorts of issues with direct reports so keeping it simple

--- app/models/organization_membership.rb
+++ app/models/organization_membership.rb
@@ -1,4 +1,11 @@
 class OrganizationMembership < ActiveRecord::Base
   belongs_to :organization
   belongs_to :user
+
+  belongs_to :reviewer, foreign_key: "reviewer_id", class_name: "User"
+
+  def direct_reports
+    ids = organization.organization_memberships.where(reviewer_id: user_id).pluck(:user_id)
+    organization.users.where(id: ids)
+  end
 end
