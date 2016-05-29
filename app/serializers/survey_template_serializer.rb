adb1ed52646bc4878581873a759bbdff87f1cb9c
Stephen Bussey <steve.bussey@salesloft.com>
2016-05-28 21:34:51 -0400

    stub out attributes

--- app/serializers/survey_template_serializer.rb
+++ app/serializers/survey_template_serializer.rb
@@ -1,7 +1,17 @@
 class SurveyTemplateSerializer < Plain::SurveyTemplateSerializer
+  attributes :response_count, :users_in_scope
+
   has_many :questions, serializer: Plain::QuestionSerializer
 
   def questions
     object.ordered_questions
+  end
+
+  def users_in_scope
+    42
+  end
+
+  def response_count
+    42
   end
 end
