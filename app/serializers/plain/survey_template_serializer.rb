4040dd79cdca30bb6cbeb25264df72f9ea072c58
Stephen Bussey <steve.bussey@salesloft.com>
2016-06-05 00:15:45 -0400

    include new attributes

--- app/serializers/plain/survey_template_serializer.rb
+++ app/serializers/plain/survey_template_serializer.rb
@@ -1,3 +1,3 @@
 class Plain::SurveyTemplateSerializer < ActiveModel::Serializer
-  attributes :id, :name, :created_at, :updated_at, :active, :recurring, :goals_section
+  attributes :id, :name, :created_at, :updated_at, :active, :recurring, :goals_section, :next_due_at, :weeks_between_due
 end
