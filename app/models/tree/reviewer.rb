e06b43f6941524bbff7c149dbb449eeee74cfc3e
Stephen Bussey <steve.bussey@salesloft.com>
2016-06-12 12:03:27 -0400

    turn the organization into a tree object based on the target you're getting information about

---
+++ app/models/tree/reviewer.rb
@@ -0,0 +1,102 @@
+module Tree
+  class Reviewer
+    def initialize(target)
+      @target = target
+      @organization = target.organization
+      @memberships = organization.organization_memberships.includes(:reviewer)
+    end
+
+    def all_parents
+      built_tree.parents.map(&:membership)
+    end
+
+    def all_reports
+      built_tree.all_children.map(&:membership)
+    end
+
+    def direct_reports
+      built_tree.children.map(&:membership)
+    end
+
+    private
+
+    attr_reader :organization, :memberships, :target
+
+    def built_tree
+      @built_tree ||= begin
+        tree = Node.new(target)
+        build_parent_nodes(tree)
+        build_child_nodes(tree)
+      end
+    end
+
+    def build_parent_nodes(tree)
+      current_node = tree
+
+      while current_node.membership.reviewer
+        current_node.parent = Node.new(current_node.membership.reviewer)
+        current_node = current_node.parent
+      end
+
+      tree
+    end
+
+    def build_child_nodes(tree)
+      frontier = [tree]
+
+      while node = frontier.shift
+        child_memberships = memberships_under_reviewer(node.membership)
+        child_memberships.each do |membership|
+          child_node = Node.new(membership)
+          child_node.parent = node
+          frontier << child_node
+        end
+      end
+
+      tree
+    end
+
+    def memberships_under_reviewer(reviewer)
+      memberships.select { |m| m.reviewer == reviewer }
+    end
+  end
+
+  class Node
+    attr_accessor :membership, :children, :parent
+
+    def initialize(membership)
+      @membership = membership
+      @parent = nil
+      @children = []
+    end
+
+    def parent=(node)
+      node.children << self
+      @parent = node
+    end
+
+    def parents
+      arr = []
+      node = parent
+
+      while node
+        arr << node
+        node = node.parent
+      end
+
+      arr
+    end
+
+    def all_children
+      arr = []
+      frontier = [self]
+
+      while node = frontier.shift
+        arr << node.children
+        frontier = frontier + node.children
+      end
+
+      arr.flatten
+    end
+  end
+end
---
+++ spec/models/tree/reviewer_spec.rb
@@ -0,0 +1,87 @@
+require "rails_helper"
+
+RSpec.describe Tree::Reviewer do
+  let!(:organization) { FactoryGirl.create(:organization) }
+  let!(:separate_root) { FactoryGirl.create(:organization_membership, organization: organization) }
+  let!(:root) { FactoryGirl.create(:organization_membership, organization: organization) }
+  let!(:root_child) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: root) }
+  let!(:me) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: root_child) }
+  let!(:child1) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: me) }
+  let!(:child2) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: me) }
+  let!(:child1_child) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: child1) }
+  let!(:child1_child_child) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: child1_child) }
+  let!(:child1_child_child_child) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: child1_child_child) }
+  let!(:child2_child) { FactoryGirl.create(:organization_membership, organization: organization, reviewer: child2) }
+
+  let(:target) { me }
+
+  subject { Tree::Reviewer.new(target) }
+
+  describe ".all_parents" do
+    it "provides a list of parents" do
+      expect(subject.all_parents).to eq([root_child, root])
+    end
+
+    context "with a rooted tree" do
+      let(:target) { root }
+
+      it "is an empty array" do
+        expect(subject.all_parents).to eq([])
+      end
+    end
+
+    context "with a single element tree" do
+      let(:target) { separate_root }
+
+      it "is an empty array" do
+        expect(subject.all_parents).to eq([])
+      end
+    end
+  end
+
+  describe ".direct_reports" do
+    it "provides a list of direct children" do
+      expect(subject.direct_reports).to eq([child1, child2])
+    end
+
+    context "with a rooted tree" do
+      let(:target) { root }
+
+      it "is the single report" do
+        expect(subject.direct_reports).to eq([root_child])
+      end
+    end
+
+    context "with a single element tree" do
+      let(:target) { separate_root }
+
+      it "is an empty array" do
+        expect(subject.direct_reports).to eq([])
+      end
+    end
+  end
+
+  describe ".all_reports" do
+    it "provides a list of children" do
+      expect(subject.all_reports.count).to eq(6)
+      expect(subject.all_reports).to eq([child1, child2, child1_child, child2_child, child1_child_child, child1_child_child_child])
+    end
+
+    context "with a rooted tree" do
+      let(:target) { root }
+
+      it "is all reports" do
+        expect(subject.all_reports.count).to eq(8)
+        expect(subject.all_reports).to eq([root_child, me, child1, child2, child1_child, child2_child, child1_child_child, child1_child_child_child])
+      end
+    end
+
+    context "with a single element tree" do
+      let(:target) { separate_root }
+
+      it "is an empty array" do
+        expect(subject.all_reports).to eq([])
+      end
+    end
+  end
+end
