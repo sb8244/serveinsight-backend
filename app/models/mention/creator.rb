b6777938bcb0e8a14f0e28cf8cce8d50c8ffefd5
Stephen Bussey <steve.bussey@salesloft.com>
2016-06-14 23:30:18 -0400

    create mentions via service object

---
+++ app/models/mention/creator.rb
@@ -0,0 +1,19 @@
+Mention::Creator = Struct.new(:mentionable, :organization_membership) do
+  COMMENT_REGEX = /(@[a-zA-Z0-9]*)/
+
+  def call(text)
+    text.scan(COMMENT_REGEX) do |match|
+      mention_name = match[0].split("@").last
+      mentioned = organization.organization_memberships.where("LOWER(mention_name) = LOWER(?)", mention_name).first
+      next if !mentioned
+      next if mentioned == organization_membership
+      mentionable.mentions.create!(organization_membership: mentioned, mentioned_by: organization_membership)
+    end
+  end
+
+  private
+
+  def organization
+    @organization ||= organization_membership.organization
+  end
+end
---
+++ spec/models/mention/creator_spec.rb
@@ -0,0 +1,65 @@
+require "rails_helper"
+
+RSpec.describe Mention::Creator do
+  let(:organization) { organization_membership1.organization }
+  let!(:organization_membership1) { FactoryGirl.create(:organization_membership, mention_name: "Person1") }
+  let!(:organization_membership2) { FactoryGirl.create(:organization_membership, organization: organization, mention_name: "Person2") }
+  let!(:organization_membership3) { FactoryGirl.create(:organization_membership, organization: organization, mention_name: "PersonSteve4") }
+
+  subject { Mention::Creator.new(mentionable, organization_membership1) }
+
+  context "for a comment" do
+    let!(:survey_template) { FactoryGirl.create(:survey_template, iteration: 1, organization: organization) }
+    let!(:question) { FactoryGirl.create(:question, organization: organization, survey_template: survey_template, question: "First", order: 1) }
+    let!(:instance) { organization_membership1.survey_instances.create!(survey_template: survey_template, iteration: 1, due_at: Time.now) }
+    let!(:answer) do
+      instance.answers.create!(
+        organization: organization,
+        question_id: question.id,
+        question_content: question.question,
+        question_order: question.order,
+        content: "Test Answer",
+        order: 0
+      )
+    end
+
+    let!(:mentionable) { FactoryGirl.create(:comment, organization_membership: organization_membership1, commentable: answer) }
+
+    it "handles a simple mention" do
+      expect {
+        subject.call("Hi @Person2")
+      }.to change { Mention.count }.by(1)
+
+      expect(Mention.last.attributes).to include(
+        "organization_membership_id" => organization_membership2.id,
+        "mentioned_by_id" => organization_membership1.id,
+        "mentionable_id" => mentionable.id,
+        "mentionable_type" => "Comment"
+      )
+    end
+
+    it "doesn't mention the author" do
+      expect {
+        subject.call("Hi @Person1")
+      }.not_to change { Mention.count }
+    end
+
+    it "is case insensitive" do
+      expect {
+        subject.call("Hi @personsteve4")
+      }.to change { Mention.count }.by(1)
+    end
+
+    it "handles multiple mentions to the same person" do
+      expect {
+        subject.call("Hi @personsteve4 @PersonSteve4")
+      }.to change { Mention.count }.by(2)
+    end
+
+    it "handles multiple mentions to different people" do
+      expect {
+        subject.call("Hi @Person2 @PersonSteve4")
+      }.to change { Mention.count }.by(2)
+    end
+  end
+end
