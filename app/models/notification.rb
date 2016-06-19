54121e38ff6a78c48b39228cf67db864ee2d822a
Stephen Bussey <steve.bussey@salesloft.com>
2016-06-19 18:49:56 -0400

    document notification types through validation

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,8 @@
 class Notification < ActiveRecord::Base
   belongs_to :organization_membership
 
+  validates :notification_type, inclusion: { in: %w(comment mention review passup) }
+
   def self.pending
     where(status: "pending")
   end
