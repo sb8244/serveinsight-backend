702f37b8f39c6dcaf98d8843d267d3414346c85c
Stephen Bussey <steve.bussey@salesloft.com>
2016-07-06 21:58:41 -0400

    Use user friendly information in the insight due mailer

--- app/mailers/insight_mailer.rb
+++ app/mailers/insight_mailer.rb
@@ -2,6 +2,19 @@ class InsightMailer < ApplicationMailer
   def insight_due(instance)
     @instance = instance
     @questions = instance.string_questions
-    mail(to: instance.organization_membership.email, subject: 'Serve Insight: Insight due soon')
+
+    @due_string = if instance.due_at.today?
+      instance.due_at.strftime("today at %l:%m %p")
+    else
+      instance.due_at.strftime("on %B %d at %l:%m %p")
+    end
+
+    @subject_string = if instance.due_at.today?
+      "today"
+    else
+      instance.due_at.strftime("on %B %d")
+    end
+
+    mail(to: instance.organization_membership.email, subject: "Serve Insight: Insight due #{@subject_string}")
   end
 end
--- spec/mailers/previews/insight_mailer_preview.rb
+++ spec/mailers/previews/insight_mailer_preview.rb
@@ -3,4 +3,10 @@ class InsightMailerPreview < ActionMailer::Preview
     instance = SurveyInstance.first
     InsightMailer.insight_due(instance)
   end
+
+  def insight_due_today
+    instance = SurveyInstance.first
+    instance.due_at = Time.now
+    InsightMailer.insight_due(instance)
+  end
 end
