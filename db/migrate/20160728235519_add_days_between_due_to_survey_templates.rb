c7032c98979914b210286010f05c0194271d41ba
Stephen Bussey <steve.bussey@salesloft.com>
2016-07-28 20:04:43 -0400

    correct the directionality

--- db/migrate/20160728235519_add_days_between_due_to_survey_templates.rb
+++ db/migrate/20160728235519_add_days_between_due_to_survey_templates.rb
@@ -3,7 +3,7 @@ class AddDaysBetweenDueToSurveyTemplates < ActiveRecord::Migration
     add_column :survey_templates, :days_between_due, :integer
 
     SurveyTemplate.find_each do |template|
-      template.update!(days_between_due: template.weeks_between_due / 7) if template.weeks_between_due
+      template.update!(days_between_due: template.weeks_between_due * 7) if template.weeks_between_due
     end
   end
 
--- db/migrate/20160729000056_remove_column_weeks_between_due_on_survey_templates.rb
+++ db/migrate/20160729000056_remove_column_weeks_between_due_on_survey_templates.rb
@@ -6,7 +6,7 @@ class RemoveColumnWeeksBetweenDueOnSurveyTemplates < ActiveRecord::Migration
   def down
     add_column :survey_templates, :weeks_between_due, :integer
     SurveyTemplate.find_each do |template|
-      template.update!(weeks_between_due: template.days_between_due * 7) if template.days_between_due
+      template.update!(weeks_between_due: template.days_between_due / 7) if template.days_between_due
     end
   end
 end
